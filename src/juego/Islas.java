ac00da8a2963ebe2b1d261c35d5411a457c4aec7
MPogrebaj <markohpro@hotmail.com>
2024-10-28 17:01:53 -0300

    Update Islas.java
    Agrego la variable velocidad y las funciones movimiento, rebotar, tocaElBordeX, reaparecerIzq y reaparecerDer.

--- src/juego/Islas.java
+++ src/juego/Islas.java
@@ -11,13 +11,15 @@ public class Islas {
 	private int y;
 	private int ancho;
 	private int alto;
+	private int velocidad;
 	private Image islasI;
 	
-	public Islas(int x, int y, int ancho, int alto) {
+	public Islas(int x, int y, int ancho, int alto, int velocidad) {
 		this.x = x;
 		this.y = y;
 		this.ancho = ancho;
-		this.alto = alto;		
+		this.alto = alto;	
+		this.velocidad = velocidad;
 	}
 	
 	public void dibujarIslas(Entorno entorno) {
@@ -40,12 +42,40 @@ public class Islas {
 		return alto;
 	}
 
+	public int getVelocidad(){
+		return velocidad;
+	}	
+
 	public Image getImageIslas() {
 		this.islasI = Herramientas.cargarImagen("imagenes/isla.jpg");
 		return islasI;		
 	}
+	
 	public void dibujarImagenIslas(Entorno entorno) {
 		entorno.dibujarImagen(islasI, this.x, this.y, 0);
+	}
+
+	public void movimiento() {
+		this.x = this.x + this.velocidad;
+	}
+	
+	public void rebotar() {
+		this.velocidad = this.velocidad*(-1);
+	}
+	
+	public boolean tocaElBordeX() {
+		boolean tocaX = this.x - this.ancho/2 <= 0 || this.x + this.ancho/2 >= 1200;
+		return tocaX;			
+        }
+	
+	public void reaparecerIzq() {
+		if(this.x - this.ancho/2 >= 1200) 
+			this.x = (this.x - this.ancho) -1200;		
+	}
+	
+	public void reaparecerDer() {
+		if(this.x + this.ancho/2 <= 0) 
+			this.x = (this.x + this.ancho) +1200;
 	}
 
 }
