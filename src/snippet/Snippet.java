bf3c82685c07993e29f25aa8a6ed78fb7bc3fa1b
Aguusquiroga <quiroga.aguus1@gmail.com>
2024-10-29 14:52:10 -0300

    delete basura

--- src/snippet/Snippet.java
+++
@@ -1,7 +0,0 @@
-package snippet;
-
-public class Snippet {
-	Description	Resource	Path	Location	Type
-	Project 'tp-progra' has no explicit encoding set	tp-progra		/tp-progra	No explicit project encoding
-}
-
